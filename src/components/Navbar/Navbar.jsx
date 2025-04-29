bbfae69459dca3092deda5cc0860ddff7a3e0aae
João Vitor <j.vt.mota@gmail.com>
2025-04-29 13:18:07 -0300

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,13 +9,14 @@ const Navbar = () => {
   return (
     <>
       <div className="container-fluid bg-[#EAEAEA] h-16 flex items-center justify-between">
+        
         <Search />
 
         <a href="https://www.cptec.inpe.br/">
           <img className="h-12" src={Logotipo} alt="Logotipo" />
         </a>
 
-        <ul className="flex gap-4 justify-center items-center text-lg m-0 p-0">
+        <ul className="flex gap-8 justify-center items-center text-lg m-0 p-0">
           <li className="flex items-baseline gap-2">
             <img src={iconTabela} alt="" className="w-4" />
             <a href="" className="font-bold text-[#5179EF]">
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,15 +1,22 @@
 import './search.js'
+import IconDropdownCircle from '../../icons/IconDropdownCircle.svg'
 
 const Search = () => {
   return (
     <>
-      <div className="relative">
+      <div className="flex justify-center items-center">
+        <button className="bg-[#5179EF] h-10 w-10 rounded-s-lg flex items-center justify-center ">
+          <img src={IconDropdownCircle} alt="" />
+        </button>
         <input
           type=""
-          className="h-10 w-80 pl-4 m-0 rounded z-0 focus:shadow focus:outline-none"
+          className="h-10 w-56 pl-2 m-0 z-0 focus:outline-none bg-[#fff]"
           placeholder="Pesquisar Cidade..."
         />
-      </div>   
+        <button className="bg-[#5179EF] h-10 w-10 rounded-e-lg ">
+        <i className="fa-solid fa-magnifying-glass text-white"></i>
+        </button>
+      </div>  
     </>
   );
 }
